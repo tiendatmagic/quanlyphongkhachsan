5b260e019c626caee39980e3cee3a9ef5630b672
tiendatmagic <tiendatmagic8@gmail.com>
2020-07-27 09:51:02 +0700

    first commit

---
+++ QLPKS/Class/Functions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+namespace QLPKS.Class
+{
+    class Functions
+    {
+        public static SqlConnection conn; // biến conn để kết nối đến csdl
+
+        public static void Connect()
+        {
+            conn = new SqlConnection("Data Source=DESKTOP-2A53AVE;Initial Catalog=QUANLYPHONGKHACHSAN;Integrated Security=True");
+            conn.Open();
+            if (conn.State == ConnectionState.Open)
+                MessageBox.Show("Bạn Đã Kết nối Thành công !", " Wellcome To !");
+            else MessageBox.Show("Kết nối không thành công");
+        }
+        public static DataTable GetDataToTable(string sql)
+        {
+            //Connect();
+            SqlDataAdapter MyData = new SqlDataAdapter(sql, conn);
+            DataTable table = new DataTable();
+            MyData.Fill(table);
+            return table;
+        }
+        public static void RunSqlDel(string sql)
+        {
+            SqlCommand cmd = new SqlCommand();
+            //cmd.Connection = Functions.Con;
+            cmd.Connection = Functions.conn;
+            cmd.CommandText = sql;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Dữ liệu đang được dùng, không thể xoá...", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                MessageBox.Show(ex.ToString());
+            }
+            cmd.Dispose();
+            cmd = null;
+        }
+        public static void RunSQL(string sql)
+        {
+            SqlCommand cmd; //Đối tượng thuộc lớp SqlCommand
+            cmd = new SqlCommand();
+            //cmd.Connection = Con; 
+            cmd.Connection = conn;
+            //Gán kết nối
+            cmd.CommandText = sql; //Gán lệnh SQL
+            try
+            {
+                cmd.ExecuteNonQuery(); //Thực hiện câu lệnh SQL
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            cmd.Dispose();//Giải phóng bộ nhớ
+            cmd = null;
+        }
+        public static bool CheckKey(string sql)
+        {
+            SqlDataAdapter MyData = new SqlDataAdapter(sql, conn);
+            DataTable table = new DataTable();
+            MyData.Fill(table);
+            if (table.Rows.Count > 0)
+                return true;
+            else return false;
+        }
+
+    }
+}
---
+++ QLPKS/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLPKS.Class;
+
+namespace QLPKS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable tblCL;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet1.CHITIETTHUEPHONG' table. You can move, or remove it, as needed.
+            //this.cHITIETTHUEPHONGTableAdapter.Fill(this.dataSet1.CHITIETTHUEPHONG);
+            // TODO: This line of code loads data into the 'dataSet1.KHACH' table. You can move, or remove it, as needed.
+            //this.kHACHTableAdapter.Fill(this.dataSet1.KHACH);
+            Functions.Connect();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult D = MessageBox.Show("Bạn có muốn thoát chương trình?", "Thoát chương trình", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (D == DialogResult.Yes)
+            {
+                this.Close();//thoát Form
+                Application.Exit();//Thoát ứng dụng
+            }
+        }
+        void Loadbang()
+        {
+            string sql;
+            sql = "SELECT * FROM CHITIETHOADON";
+            tblCL = Functions.GetDataToTable(sql);
+            dataGridView1.DataSource = tblCL;
+            dataGridView1.Columns[0].HeaderText = "Số HD";
+            dataGridView1.Columns[1].HeaderText = "Số Phòng";
+            dataGridView1.Columns[3].HeaderText = "Đon Giá";
+            dataGridView1.Columns[3].HeaderText = "Số Ngày";
+            dataGridView1.Columns[3].HeaderText = "Số Tiền Trả";
+            dataGridView1.Columns[0].Width = 120;
+            dataGridView1.Columns[1].Width = 150;
+            dataGridView1.Columns[2].Width = 120;
+            dataGridView1.Columns[3].Width = 120;
+            dataGridView1.AllowUserToAddRows = false; //Không cho người dùng thêm dữ liệu trực tiếp
+            dataGridView1.EditMode = DataGridViewEditMode.EditProgrammatically; //Không cho sửa dữ liệu trực tiếp}
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            if (tblCL.Rows.Count == 0) //Nếu không có dữ liệu
+            {
+                MessageBox.Show("Không có dữ liệu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            textBox1.Text = dataGridView1.CurrentRow.Cells["SOHD"].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            textBox1.Enabled = true;
+            string sql; //Lưu câu lệnh sql
+            if (tblCL.Rows.Count == 0)
+            {
+                MessageBox.Show("Bạn có muốn thêm thông tin vào trong cơ sở dữ liệu không", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            sql = @"INSERT INTO CHITIETHOADON (SOHD) 
+                    VALUES (N'" + textBox1.Text + @"')";
+            Class.Functions.RunSQL(sql);
+            Loadbang();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string sql; //Lưu câu lệnh sql
+            if (tblCL.Rows.Count == 0)
+            {
+                MessageBox.Show("Không còn dữ liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (MessageBox.Show("Bạn có muốn xoá không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                sql = "DELETE CHITIETHOADON WHERE SOHD=N'" + dataGridView1.CurrentRow.Cells["SOHD"].Value.ToString() + "'";
+                Class.Functions.RunSqlDel(sql);
+                Loadbang();
+                //ResetValue();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Loadbang();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sql; //Lưu câu lệnh sql
+            if (tblCL.Rows.Count == 0)
+            {
+                MessageBox.Show("Không còn dữ liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (textBox1.Text == "") //nếu chưa chọn bản ghi nào
+            {
+                MessageBox.Show("Bạn chưa chọn bản ghi nào", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            sql = ("UPDATE CHITIETHOADON SET SOHD=N'" + textBox1.Text.ToString() + "' WHERE SOHD=N'" + dataGridView1.CurrentRow.Cells["SOHD"].Value.ToString() + "'");
+            Class.Functions.RunSQL(sql);
+            Loadbang();
+
+         }
+    }
+}
